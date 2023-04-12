1a799bda9586f67bee36c49daa2a6fcf77b0c9b0
AnastaciaS <65757424+AnastaciaS@users.noreply.github.com>
2023-04-12 22:50:33 +0200

    fixes and formating

--- dom-button.js
+++ dom-button.js
@@ -1,45 +1,48 @@
 'use strict';
 const button = document.querySelector('.btn');
-		const lastStateMsg = document.querySelector('#lastState');
+const lastStateMsg = document.querySelector('#lastState');
 
-		// Set initial state
-		let isOn = false;
+// Set initial state
+let isOn = false;
+button.textContent = 'Turn off';
+document.body.style.backgroundColor = 'lightgray';
+
+// Check if there is a saved state
+
+if (localStorage.getItem('isOn')) {
+	isOn = localStorage.getItem('isOn') === 'true';
+
+	if (isOn) {
+		button.textContent = 'Turn on';
+		document.body.style.backgroundColor = 'darkgray';
+		const lastOnTime = localStorage.getItem('lastOnTime');
+		lastStateMsg.textContent = `Last turn on: ${lastOnTime}`;
+	} else {
+		const lastOffTime = localStorage.getItem('lastOffTime');
+		lastStateMsg.textContent = `Last turn off: ${lastOffTime}`;
+	}
+}
+
+// Add event listener to the button
+button.addEventListener('click', function () {
+	// Update state and button text
+	isOn = !isOn;
+
+	if (isOn) {
+		button.textContent = 'Turn on';
+		document.body.style.backgroundColor = 'darkgray';
+		// Save last on time and update message
+		const lastOnTime = new Date().toLocaleString();
+		localStorage.setItem('isOn', 'true');
+		localStorage.setItem('lastOnTime', lastOnTime);
+		lastStateMsg.textContent = `Last turn on: ${lastOnTime}`;
+	} else {
 		button.textContent = 'Turn off';
 		document.body.style.backgroundColor = 'lightgray';
-
-		// Check if there is a saved state
-		if (localStorage.getItem('isOn')) {
-			isOn = localStorage.getItem('isOn') === 'true';
-			if (isOn) {
-				button.textContent = 'Turn on';
-				document.body.style.backgroundColor = 'darkgray';
-				const lastOnTime = localStorage.getItem('lastOnTime');
-				lastStateMsg.textContent = `Last turn on: ${lastOnTime}`;
-			} else {
-				const lastOffTime = localStorage.getItem('lastOffTime');
-				lastStateMsg.textContent = `Last turn off: ${lastOffTime}`;
-			}
-		}
-
-		// Add event listener to the button
-		button.addEventListener('click', function() {
-			// Update state and button text
-			isOn = !isOn;
-			if (isOn) {
-				button.textContent = 'Turn on';
-				document.body.style.backgroundColor = 'darkgray';
-				// Save last on time and update message
-				const lastOnTime = new Date().toLocaleString();
-				localStorage.setItem('isOn', 'true');
-				localStorage.setItem('lastOnTime', lastOnTime);
-				lastStateMsg.textContent = `Last turn on: ${lastOnTime}`;
-			} else {
-				button.textContent = 'Turn off';
-				document.body.style.backgroundColor = 'lightgray';
-				// Save last off time and update message
-				const lastOffTime = new Date().toLocaleString();
-				localStorage.setItem('isOn', 'false');
-				localStorage.setItem('lastOffTime', lastOffTime);
-				lastStateMsg.textContent = `Last turn off: ${lastOffTime}`;
-			}
-		});
+		// Save last off time and update message
+		const lastOffTime = new Date().toLocaleString();
+		localStorage.setItem('isOn', 'false');
+		localStorage.setItem('lastOffTime', lastOffTime);
+		lastStateMsg.textContent = `Last turn off: ${lastOffTime}`;
+	}
+});
